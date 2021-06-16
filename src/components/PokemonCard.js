5417115ba1f26d1a08dd7520c6abf689ab38a3d1
caroline223 <67647836+caroline223@users.noreply.github.com>
2021-06-16 01:38:28 -0400

    Updated the page, collection, and card pages

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,20 +2,32 @@ import React from 'react'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCard extends React.Component {
+  
+  state = {
+    side: 'front'
+  }
+
+
+  handleClick = () => {
+    //toggle between the front and back of each card
+    this.state.side === 'back' ? this.setState({side: 'front'}) : this.setState({side: 'back'})
+  }
+  
   render() {
+    const {name, hp, sprites} = this.props.pokemon;
     return (
       <Card>
         <div>
-          <div className="image">
-            <img alt="oh no!" />
+          <div onClick={ this.handleClick } className="image">
+            <img src={this.state.side === 'back' ? sprites.back: sprites.front }alt="oh no!" />
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{ name }</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              { hp } hp
             </span>
           </div>
         </div>
--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -5,16 +5,61 @@ import Search from './Search'
 import { Container } from 'semantic-ui-react'
 
 class PokemonPage extends React.Component {
+
+  state = {
+    pokemon: [],
+    renderedPokemon: [],
+    searchInput: ""
+  }
+
+  componentDidMount(){
+    //On mount, fetch initial information to the page
+    fetch('http://localhost:3000/pokemon')
+    .then(response => response.json())
+    .then(this.setInitialPokemon)
+  }
+
+  setInitialPokemon = (pokemonCollection) => {
+    //upon initial render to the page, shows the entire pokemon collection
+    this.setState({
+      pokemon:[...pokemonCollection],
+      renderedPokemon: [...pokemonCollection]
+    })
+  }
+
+
+
+  handleFormSubmit = (event) => {
+
+  }
+
+  filterSearchByInput = (input) => {
+    this.state.pokemon.filter(pokemon => pokemon.name.includes(input))
+  }
+
+  handleSearchInput = (event) => {
+    //allows the browser to filter the pokemon search by query. If empty, will show all of the cards
+    this.setState({
+      searchInput: event.target.value
+    })
+       if(event.target.value === ""){
+         this.setState({renderedPokemon: [...this.state.pokemon]})
+       } else{
+          this.setState({renderedPokemon: this.filterSearchByInput(event.target.value)})
+       }
+  }
+
+
   render() {
     return (
       <Container>
         <h1>Pokemon Searcher</h1>
         <br />
-        <PokemonForm />
+        <PokemonForm handleFormSubmit={this.handleFormSubmit}/>
         <br />
-        <Search />
+        <Search handleSearchInput={this.handleSearchInput}/>
         <br />
-        <PokemonCollection />
+        <PokemonCollection pokemon={this.state.renderedPokemon} />
       </Container>
     )
   }
